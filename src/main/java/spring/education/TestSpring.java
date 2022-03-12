aa9e65ffa7e158b50d7f9959de581b646617fa4c
Il'dar <yumagulov.ildar@gmail.com>
2022-03-12 14:06:52 +0500

    1stpush

---
+++ src/main/java/spring/education/TestSpring.java
@@ -0,0 +1,13 @@
+package spring.education;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestSpring {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+                "applicationContext.xml");
+        TestBean tb = context.getBean("testBean",TestBean.class);
+        System.out.println(tb.getName());
+        context.close();
+    }
+}
