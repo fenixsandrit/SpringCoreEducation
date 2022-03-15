b44ded2daa071c40dcffd2edb2f11b461c8204ba
Il'dar <yumagulov.ildar@gmail.com>
2022-03-15 23:19:45 +0500

    factoryMethod

--- src/main/java/spring/education/ClassicalMusic.java
+++ src/main/java/spring/education/ClassicalMusic.java
@@ -1,6 +1,13 @@
 package spring.education;
 
 public class ClassicalMusic implements Music{
+
+    private ClassicalMusic(){};
+
+    public static ClassicalMusic getInstance(){
+        return new ClassicalMusic();
+    }
+
     private void doMyInit(){
         System.out.println("Classical music init method");
     }
