8af2307d071e869a9515d7321427074d765b8ec4
Il'dar <yumagulov.ildar@gmail.com>
2022-03-15 22:46:33 +0500

    learnBeanScopes

--- src/main/java/spring/education/TestSpring.java
+++ src/main/java/spring/education/TestSpring.java
@@ -6,11 +6,15 @@ public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext.xml");
-        MusicPlayer musicPlayer = context.getBean("musicPlayer",MusicPlayer.class);
-        musicPlayer.playMusic();
-        System.out.println(musicPlayer.getVolume());
-        System.out.println(musicPlayer.getName());
-        musicPlayer.playMusics();
+        MusicPlayer musicPlayer1 = context.getBean("musicPlayer1",MusicPlayer.class);
+        MusicPlayer musicPlayer2 = context.getBean("musicPlayer1",MusicPlayer.class);
+        MusicPlayer musicPlayer3 = context.getBean("musicPlayer2",MusicPlayer.class);
+        MusicPlayer musicPlayer4 = context.getBean("musicPlayer2",MusicPlayer.class);
+
+        //out = true
+        System.out.println(musicPlayer1 == musicPlayer2);
+        //out = false
+        System.out.println(musicPlayer3 == musicPlayer4);
 
         context.close();
     }
