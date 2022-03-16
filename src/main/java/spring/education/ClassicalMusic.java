e8d2947cced87de3cb695f2b9f4bf5ff04a015a0
Il'dar <yumagulov.ildar@gmail.com>
2022-03-16 22:51:32 +0500

    Annotation_PostConstructAndPreDestroy

--- src/main/java/spring/education/ClassicalMusic.java
+++ src/main/java/spring/education/ClassicalMusic.java
@@ -2,8 +2,20 @@ package spring.education;
 
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
+@Component
 public class ClassicalMusic implements Music{
+
+    @PostConstruct
+    private void doMyInit(){
+        System.out.println("Classical music init method");
+    }
+    @PreDestroy
+    private void doMyDestroy(){
+        System.out.println("Classical music destroy method");
+    }
 
     @Override
     public String getSong() {
--- src/main/java/spring/education/TestSpring.java
+++ src/main/java/spring/education/TestSpring.java
@@ -8,14 +8,8 @@ public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext.xml");
-        Music m1 = context.getBean("rockMusic",RockMusic.class);
-        Music m2 = context.getBean("rockMusic",RockMusic.class);
-        Music m3 = context.getBean("rapMusic",RapMusic.class);
-        Music m4 = context.getBean("rapMusic",RapMusic.class);
-        //Scope prototype
-        System.out.println(m1 == m2);
-        //Scope singleton
-        System.out.println(m3 == m3);
+        Music classicalMusic = context.getBean("classicalMusic",ClassicalMusic.class);
+        System.out.println(classicalMusic.getSong());
         context.close();
     }
 }
