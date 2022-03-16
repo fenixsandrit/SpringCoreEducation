61bd289d8d69a65143fea445813dcde2e2879b5a
Il'dar <yumagulov.ildar@gmail.com>
2022-03-16 22:25:17 +0500

    DI_WithAnnotationQualifier

--- src/main/java/spring/education/MusicPlayer.java
+++ src/main/java/spring/education/MusicPlayer.java
@@ -2,6 +2,7 @@ package spring.education;
 
 import org.springframework.aop.target.LazyInitTargetSource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
@@ -9,17 +10,18 @@ import java.util.List;
 @Component
 public class MusicPlayer {
 
-    private RapMusic rapMusic;
-    private RockMusic rockMusic;
+    private Music music1,music2;
+
     @Autowired
-    public MusicPlayer(RockMusic rockMusic, RapMusic rapMusic) {
-        this.rapMusic = rapMusic;
-        this.rockMusic = rockMusic;
+    public MusicPlayer(@Qualifier("rapMusic") Music music1,
+                       @Qualifier("rockMusic") Music music2){
+        this.music1 = music1;
+        this.music2 = music2;
     }
 
 
     public String playMusic(){
-        return ("Playing: " + rapMusic.getSong() + "\n     also playing: " + rockMusic.getSong());
+        return ("Playing: " + music1.getSong() + "\n" + "  also playing: " + music2.getSong());
     }
 
 }
